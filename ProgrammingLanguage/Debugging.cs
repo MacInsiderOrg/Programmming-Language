76f2e41d6a51f416f8b3900cce506dbf9d61f07e
Andrew <macinsiderorg@gmail.com>
2015-12-15 00:56:50 +0200

    Deleted a comment

--- ProgrammingLanguage/Debugging.cs
+++ ProgrammingLanguage/Debugging.cs
@@ -32,48 +32,6 @@ namespace ProgrammingLanguage
         public static void OnAddVariablesToList()
         {
             AddVariablesToList?.Invoke(null, new EventArguments() { Variables = VariablesList });
-        }
-        
-        /*
-        //private static StreamWriter debugFile;
-
-        public static void StartDebugging()
-        {
-            debugFile = new StreamWriter("debug.txt");
-            WriteToDebug("This is debug file, which contain all information about your compilation file\r\n");
-        }
-
-        public static void EndDebugging()
-        {
-            debugFile.Close();
-        }
-
-        public static void WriteToDebug(string message)
-        {
-            debugFile.WriteLine(message);
-        }
-
-        public static void WriteTokensToDebug(List<Tokens.Token> tokens)
-        {
-            WriteToDebug("Tokens in current program:\r\n");
-            for (int index = 0; index < tokens.Count; index++)
-            {
-                debugFile.WriteLine("{0}: {1}", index, tokens[index].ToString());
-            }
-
-            //WriteTokensToList(tokens);
-        }
-
-        public static void WriteVariablesToDebug(Dictionary<string, Variable> variables)
-        {
-            WriteToDebug("\r\nVariables with values in current program:\r\n");
-
-            foreach (var variable in variables)
-            {
-                debugFile.WriteLine(string.Format("[{0}] {1} = {2}", variable.Value.Type, variable.Key, variable.Value.Value));
-            }
-
-            //VariablesList = variables;
-        }*/
+        }        
     }
 }
